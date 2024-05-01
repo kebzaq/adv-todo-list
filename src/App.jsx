8e4614a1202991ad4b30703624a49ce625347c8f
Azamat Gaparov <agaparovkg@gmail.com>
2024-05-01 16:57:05 -0700

    updated readme

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, InputGroup, Input, InputGroupText, List } from "reactstrap";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <h1>Todo List React App</h1>
+      <InputGroup>
+        <Input value="" />
+        <Button color="primary">Add Todo</Button>
+      </InputGroup>
+      <main>
+        <List style={{ padding: "0" }}>
+          <InputGroup style={{ marginBottom: "2px" }}>
+            <InputGroupText>
+              <Input
+                addon
+                aria-label="Checkbox for following text input"
+                type="checkbox"
+              />
+            </InputGroupText>
+            <Input value="Todo 1" />
+            <Button color="primary">Save</Button>
+            <Button color="secondary">Edit</Button>
+            <Button color="danger">Delete</Button>
+          </InputGroup>
+        </List>
+      </main>
+      <footer>
+        <Button color="danger">Clear All Todos</Button>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
