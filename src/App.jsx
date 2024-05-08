24677e80bc078d3fc31792ea67a41d3cd47581a4
Azamat Gaparov <agaparovkg@gmail.com>
2024-05-07 19:01:25 -0700

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+import { InputGroup, Input, Button, List, InputGroupText } from "reactstrap";
 import "./App.css";
-import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
-      <TodoList />
+      <h1>Todo List React App</h1>
+      <InputGroup>
+        <Input />
+        <Button color="primary">Add Todo</Button>
+      </InputGroup>
+      <main>
+        <List style={{ padding: "0" }}>
+          <InputGroup style={{ marginBottom: "2px" }}>
+            <InputGroupText>
+              <Input
+                addon
+                aria-label="Checkbox for following text input"
+                type="checkbox"
+              />
+            </InputGroupText>
+            <Input value="Todo 1" onChange={() => {}} />
+            <Button color="primary">Save</Button>
+            <Button color="secondary">Edit</Button>
+            <Button color="danger">Delete</Button>
+          </InputGroup>
+        </List>
+      </main>
+      <footer>
+        <Button color="danger">Clear Completed Todos</Button>
+      </footer>
     </div>
   );
 }
