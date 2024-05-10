247f4a3c959d2907a43ee385258155e829131ca5
Kazbek Abraliev <kazbek.abraliev@gmail.com>
2024-05-10 19:46:49 -0400

    adv todo list react app

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,122 @@
 import { InputGroup, Input, Button, List, InputGroupText } from "reactstrap";
 import "./App.css";
+import { useState } from "react";
+import SingleTodo from "./components/SingleTodo";
+import { v4 as uuidv4 } from "uuid";
+
+//LS-1: creating var to GET data from localStorage using JSON parse
+const localStorageTodoList = JSON.parse(localStorage.getItem("todoList"));
+//LS-2: no checking id localStorage has data, if not then create empty array...
+const initialTodoList =
+  localStorageTodoList !== null ? localStorageTodoList : [];
 
 function App() {
+  const [inputTask, setInputTask] = useState("");
+  const [todoList, setTodoList] = useState(initialTodoList);
+  const [editId, setEditId] = useState(null);
+
+  //LS-3 update LocalStorage
+  const updateLocalStorage = (newTodoList) => {
+    localStorage.setItem("todoList", JSON.stringify(newTodoList));
+  };
+  //handle input value
+  const handleChange = (e) => {
+    setInputTask(e.target.value);
+  };
+  // add new todo
+  const addTodo = () => {
+    //LS-3 create deep copy of toList from useState
+    const newTodoList = JSON.parse(JSON.stringify(todoList));
+    //
+    const newId = newTodoList.length === 0 ? 1 : newTodoList.length - 1 + 1;
+    if (inputTask.trim() === "") {
+      alert("Enter your task");
+    } else {
+      // create new Task  - id using uuid
+      const newTask = {
+        id: uuidv4(),
+        task: inputTask.trim(),
+        completed: false,
+      };
+      newTodoList.push(newTask);
+      setTodoList(newTodoList);
+    }
+    updateLocalStorage(newTodoList);
+    setInputTask("");
+  };
+  const handleDelete = (id) => {
+    const newTodoList = todoList.filter((todo) => todo.id !== id);
+    setTodoList(newTodoList);
+    updateLocalStorage(newTodoList);
+  };
+  const handleEdit = (id) => {
+    setEditId(id);
+  };
+
+  const handleSave = (editId, updateTask) => {
+    const tempList = JSON.parse(JSON.stringify(todoList));
+    if (updateTask.trim() === "") {
+      alert("Enter your task");
+    }
+
+    tempList.forEach((el) => {
+      if (el.id === editId) {
+        el.task = updateTask;
+      }
+    });
+
+    setTodoList(tempList);
+    updateLocalStorage(tempList);
+    setEditId(null);
+  };
+
+  const handleCompleted = (e, id) => {
+    const tempList = JSON.parse(JSON.stringify(todoList));
+
+    tempList.forEach((el) => {
+      if (el.id === id) {
+        el.completed = e.target.checked;
+      }
+    });
+
+    setTodoList(tempList);
+    updateLocalStorage(tempList);
+  };
+  const handleClearCompletedTodos = () => {
+    const incompleteTodos = todoList.filter((todo) => !todo.completed);
+    updateLocalStorage(incompleteTodos);
+    setTodoList(incompleteTodos);
+  };
+
+  //
   return (
     <div className="App">
       <h1>Todo List React App</h1>
       <InputGroup>
-        <Input />
-        <Button color="primary">Add Todo</Button>
+        <Input value={inputTask} onChange={handleChange} />
+        <Button color="primary" onClick={addTodo}>
+          Add Todo
+        </Button>
       </InputGroup>
       <main>
         <List style={{ padding: "0" }}>
-          <InputGroup style={{ marginBottom: "2px" }}>
-            <InputGroupText>
-              <Input
-                addon
-                aria-label="Checkbox for following text input"
-                type="checkbox"
-              />
-            </InputGroupText>
-            <Input value="Todo 1" onChange={() => {}} />
-            <Button color="primary">Save</Button>
-            <Button color="secondary">Edit</Button>
-            <Button color="danger">Delete</Button>
-          </InputGroup>
+          {todoList.map((todo) => {
+            const oneProps = {
+              ...todo,
+              editId,
+              handleDelete,
+              handleEdit,
+              handleSave,
+              handleCompleted,
+            };
+            return <SingleTodo {...oneProps} key={todo.id} />;
+          })}
         </List>
       </main>
       <footer>
-        <Button color="danger">Clear Completed Todos</Button>
+        <Button color="danger" onClick={handleClearCompletedTodos}>
+          Clear Completed Todos
+        </Button>
       </footer>
     </div>
   );
---
+++ src/components/SingleTodo.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { InputGroup, Input, Button, List, InputGroupText } from "reactstrap";
+import { useState } from "react";
+
+export default function SingleTodo({
+  id,
+  task,
+  completed,
+  editId,
+  handleDelete,
+  handleEdit,
+  handleSave,
+  handleCompleted,
+}) {
+  const [updateTask, setUpdateTask] = useState(task);
+  const handleUpdateTask = (e) => {
+    setUpdateTask(e.target.value);
+  };
+  const isDisabled = () => true;
+  return (
+    <div>
+      <InputGroup style={{ marginBottom: "2px" }}>
+        <InputGroupText>
+          <Input
+            addon
+            aria-label="Checkbox for following text input"
+            type="checkbox"
+            checked={completed}
+            onChange={(e) => {
+              handleCompleted(e, id);
+            }}
+          />
+        </InputGroupText>
+        <Input
+          className={completed ? "cross capital" : "capital"}
+          disabled={editId !== id}
+          value={updateTask}
+          onChange={handleUpdateTask}
+        />
+        {/* show SAVE if editId === null, and show EDIT if it is not null */}
+        {editId === id ? (
+          <Button
+            color="primary"
+            onClick={() => {
+              handleSave(editId, updateTask);
+            }}
+          >
+            Save
+          </Button>
+        ) : (
+          <Button
+            color="secondary"
+            onClick={() => {
+              handleEdit(id);
+            }}
+          >
+            Edit
+          </Button>
+        )}
+
+        <Button
+          color="danger"
+          onClick={() => {
+            handleDelete(id);
+          }}
+        >
+          Delete
+        </Button>
+      </InputGroup>
+    </div>
+  );
+}
